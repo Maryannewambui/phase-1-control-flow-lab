8e712a5cc9a89ae223b7b6624ed7c91e64f33f1f
Maryannewambui <trimbleleewambui@gmail.com>
2023-09-18 22:19:47 +0300

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+
+let distance
+if (feet <= 400) {
+  return "This one is on me!";
+} else if (feet > 2000 && feet <= 2500) {
+  return "I will gladly take your thirty bucks.";
+} else if (feet > 2500) {
+  return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(location) {
+  return location === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+}
+
+function switchOnCharmFromTip(reply){
+  switch (reply) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+      case 'not as generous':
+        return "Thank you.";
+        break;
+        default:
+          return "Bye."
+  }
 }
